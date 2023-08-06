0efa0ce6eb22608fec22dbd73fd18bcad3026d7b
chs98412 <chs98412@gmail.com>
2023-08-06 16:13:11 +0900

    project setting

---
+++ src/main/java/com/HappyScrolls/HappyScrollsApplication.java
@@ -0,0 +1,13 @@
+package com.HappyScrolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyScrollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyScrollsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HappyScrolls/HappyScrollsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HappyScrolls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyScrollsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
