83e2d1b34fd482ec4e5c4d8f49f238e474268928
chs98412 <chs98412@gmail.com>
2023-08-08 01:44:26 +0900

    feat: 댓글 작성, 임시조회 기능 #3 #5

---
+++ src/main/java/com/HappyScrolls/dto/CommentDTO.java
@@ -0,0 +1,38 @@
+package com.HappyScrolls.dto;
+
+import com.HappyScrolls.entity.Comment;
+import lombok.Builder;
+import lombok.Data;
+
+public class CommentDTO {
+
+
+
+    @Builder
+    @Data
+    public static class Request{
+        private Long postId;
+        private String body;
+
+        public Comment toEntity() {
+            return Comment.builder()
+                    .body(this.body)
+                    .build();
+        }
+    }
+
+
+    @Builder
+    @Data
+    public static class Response {
+        private Long id;
+        private String body;
+    }
+
+    @Builder
+    @Data
+    public static class edit {
+        private Long id;
+        private String body;
+    }
+}
--- src/main/java/com/HappyScrolls/service/CommentService.java
+++ src/main/java/com/HappyScrolls/service/CommentService.java
@@ -1,7 +1,60 @@
 package com.HappyScrolls.service;
 
+import com.HappyScrolls.dto.CommentDTO;
+import com.HappyScrolls.entity.Article;
+import com.HappyScrolls.entity.Comment;
+import com.HappyScrolls.entity.Member;
+import com.HappyScrolls.repository.ArticleRepository;
+import com.HappyScrolls.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CommentService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public CommentDTO.Response commentCreate(Member member, CommentDTO.Request request) {
+
+        Article article = articleRepository.findById(request.getPostId()).get();
+
+        Comment makeComment = request.toEntity();
+        makeComment.setArticle(article);
+        makeComment.setMember(member);
+
+        commentRepository.save(makeComment);
+
+        return CommentDTO.Response
+                .builder()
+                .id(makeComment.getId())
+                .body(makeComment.getBody())
+                .build();
+    }
+
+    public List<CommentDTO.Response> commentRetrieve(Long id) {
+        Article article = articleRepository.findById(id).get();
+
+        List<Comment> comments = commentRepository.findByArticle(article);
+
+        List<CommentDTO.Response> response = new ArrayList<>();
+        for (Comment comment : comments) {
+            response.add(CommentDTO.Response
+                    .builder()
+                    .id(comment.getId())
+                    .body(comment.getBody())
+                    .build());
+        }
+
+        return response;
+
+    }
+
+
 }
+
