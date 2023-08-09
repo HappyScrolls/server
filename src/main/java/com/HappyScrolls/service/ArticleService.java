62139742825173b871c04b7ec8b7204bfe4d2800
chs98412 <chs98412@gmail.com>
2023-08-09 18:26:52 +0900

    feat: 게시글 수정 예외처리 게시글 본인 게시물 아닐시 예외 #16

--- src/main/java/com/HappyScrolls/service/ArticleService.java
+++ src/main/java/com/HappyScrolls/service/ArticleService.java
@@ -3,6 +3,7 @@ package com.HappyScrolls.service;
 import com.HappyScrolls.dto.ArticleDTO;
 import com.HappyScrolls.entity.Article;
 import com.HappyScrolls.entity.Member;
+import com.HappyScrolls.exception.NoAuthorityExceoption;
 import com.HappyScrolls.repository.ArticleRepository;
 import com.HappyScrolls.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,10 +63,12 @@ public class ArticleService {
 
     public ArticleDTO.Response articleEdit(Member member,ArticleDTO.Edit request) {
 
-        //유저 검증 로직
 
         Article article = articleRepository.findById(request.getId()).orElseThrow(() -> new NoSuchElementException(String.format("article[%s] 게시글을 찾을 수 없습니다", request.getId()))); //%s?
 
+        if (!article.getMember().equals(member)) {
+            throw new NoAuthorityExceoption("수정 권한이 없습니다. 본인 소유의 글만 수정 가능합니다.");
+        }
 
         article.edit(request);
 
