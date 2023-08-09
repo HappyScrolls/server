9b7345010d1e1147c84e97b6f1a2345e9ee053b4
chs98412 <chs98412@gmail.com>
2023-08-10 00:31:51 +0900

    feat: 게시물 전체 조회 성공 테스트 기능 작성 #6

--- src/test/java/com/HappyScrolls/service/ArticleServiceTest.java
+++ src/test/java/com/HappyScrolls/service/ArticleServiceTest.java
@@ -70,6 +70,36 @@ public class ArticleServiceTest {
         assertThat(response.getBody()).isEqualTo(request.getBody());
     }
 
+    @Test
+    @DisplayName("게시물 전체 조회 기능이 제대로 동작하는지 확인")
+    void 게시물_전체조회_성공_테스트() {
+
+        Member member = Member.builder().id(USER_ID).email("chs98412@naver,com").nickname("hyuksoon").thumbnail("img").build();
+        Article article1 = new Article(1l, member, "제목1", "내용1");
+        Article article2 = new Article(2l, member, "제목2", "내용2");
+        Article article3 = new Article(3l, member, "제목3", "내용3");
+
+        List<Article> articles = new ArrayList<>();
+        articles.add(article1);
+        articles.add(article2);
+        articles.add(article3);
+
+        when(articleRepository.findAll()).thenReturn(articles);
+
+
+        List<ArticleDTO.Response> response = articleService.articleRetrieveAll();
+
+        verify(articleRepository).findAll();
+
+
+        assertThat(response).isEqualTo(articles.stream()
+                .map(article -> ArticleDTO.Response.builder()
+                        .id(article.getId())
+                        .title(article.getTitle())
+                        .body(article.getBody())
+                        .build())
+                .collect(Collectors.toList()));
+    }
 
 
 }
