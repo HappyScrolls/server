afc828c19e7ae3321c0f86e907eb21b3a25c9440
chs98412 <chs98412@gmail.com>
2023-08-23 00:12:03 +0900

    feat: Cart 엔티티 설계

---
+++ src/main/java/com/HappyScrolls/entity/Cart.java
@@ -0,0 +1,29 @@
+package com.HappyScrolls.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+public class Cart {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private Product product;
+
+}
